7d8df5defca2cdf80cfce37747fa1f65f55a91de
Carlos Marques <catralitos11@gmail.com>
2023-08-27 00:51:11 +0100

    Changed default mech parts, and made code to get a random part so we can spawn them on enemy death

---
+++ Assets/_Scripts/Managers/PartsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using _Scripts.MechaParts.SO;
+using UnityEngine;
+
+namespace _Scripts.Managers
+{
+    public class PartsManager : MonoBehaviour
+    {
+        public static PartsManager Instance { get; private set; }
+
+        [SerializeField] private List<MechPart> parts;
+        [SerializeField] private Inventory inventory;
+        
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogError($"There's more than one Parts! {transform} - {Instance}");
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+        public MechPart GetRandomPart()
+        {
+            int c = 0;
+
+            while (c < 1000)
+            {
+                MechPart randomPart = parts[Random.Range(0, parts.Count)];
+                if (!inventory.Contains(randomPart)) return randomPart;
+                c++;
+            }
+
+            return null;
+        }
+        
+    }
+}
