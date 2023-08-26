14484511131fab056db57a14f9f8d0c2dde718ec
Carlos Marques <catralitos11@gmail.com>
2023-08-26 18:53:27 +0100

    Created placeholder credits scene

---
+++ Assets/_Scripts/UI/CreditsScreenManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /// <summary>
+    /// The start game button
+    /// </summary>
+    [Header("Buttons")] public Button backButton;
+
+    /// <summary>
+    /// The tutorial button
+    /// </summary>
+    public Button exitButton;
+
+    private void Start()
+    {
+        backButton.onClick.AddListener(BackToTitle);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    private static void BackToTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private static void ExitGame()
+    {
+        Application.Quit();
+    }
+}
