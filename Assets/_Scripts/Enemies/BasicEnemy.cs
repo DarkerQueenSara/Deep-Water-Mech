7de75db0274b435705b3dad4d5c7d5ca5b0a1e3d
José Siopa <siopajose@outlook.com>
2023-08-26 22:55:08 +0100

    Enemy complete

--- Assets/_Scripts/Enemies/BasicEnemy.cs
+++ Assets/_Scripts/Enemies/BasicEnemy.cs
@@ -12,27 +12,38 @@ namespace _Scripts.Enemies
         [SerializeField] private Transform player;
         [SerializeField] private LayerMask groundLayer, playerLayer;
         [SerializeField] private int maxHealth;
-        
-        [Header("Patrolling")]
-        [SerializeField] private Vector3 walkPoint;
+        [SerializeField] private Animator animator;
+
+        [Header("Patrolling")] [SerializeField]
+        private Vector3 walkPoint;
+
         [SerializeField] private float walkPointRange;
 
-        [Header("Attacking")] 
-        [SerializeField] private float timeBetweenAttacks;
+        [Header("Attacking")] [SerializeField] private float timeBetweenAttacks;
         [SerializeField] private bool isRanged;
         [SerializeField] private GameObject projectile;
         [SerializeField] private int damage;
+        [SerializeField] private Transform projectileSpawnPointLeft;
+        [SerializeField] private Transform projectileSpawnPointRight;
 
-        [Header("States")] 
-        [SerializeField] private float sightRange;
+        [Header("States")] [SerializeField] private float sightRange;
         [SerializeField] private float attackRange;
 
         private float _currentHealth;
         private bool _walkPointSet, _alreadyAttacked, _playerInSightRange, _playerInAttackRange;
+        private static readonly int Moving = Animator.StringToHash("Moving");
+        private static readonly int IsDead = Animator.StringToHash("IsDead");
+        private static readonly int AttackLeft = Animator.StringToHash("AttackLeft");
+        private static readonly int AttackRight = Animator.StringToHash("AttackRight");
 
         private void Awake()
         {
             _currentHealth = maxHealth;
+        }
+
+        private void Start()
+        {
+            if (player == null) player = GameObject.FindGameObjectWithTag("Mecha").transform;
         }
 
         private void Update()
@@ -47,7 +58,12 @@ namespace _Scripts.Enemies
         private void Patrolling()
         {
             if (!_walkPointSet) SearchWalkPoint();
-            if (_walkPointSet) agent.SetDestination(walkPoint);
+            if (_walkPointSet)
+            {
+                agent.SetDestination(walkPoint);
+                animator.SetBool(Moving, true);
+            }
+            else animator.SetBool(Moving, false);
 
             Vector3 dstToWalkPoint = transform.position - walkPoint;
             if (dstToWalkPoint.magnitude < 1f) _walkPointSet = false;
@@ -66,26 +82,34 @@ namespace _Scripts.Enemies
         private void ChasePlayer()
         {
             agent.SetDestination(player.position);
+            animator.SetBool(Moving, true);
         }
 
         private void AttackPlayer()
         {
             agent.SetDestination(transform.position);
+            animator.SetBool(Moving, false);
             transform.LookAt(player);
+            float originalYRotation = transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(0, originalYRotation, 0);
             if (!_alreadyAttacked)
             {
+                int rnd = Random.Range(0, 2);
+                Vector3 spawnPoint = rnd == 0 ? projectileSpawnPointLeft.position : projectileSpawnPointRight.position;
                 // Attack
                 if (isRanged)
                 {
-                    Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+                    Rigidbody rb = Instantiate(projectile, spawnPoint, Quaternion.identity)
+                        .GetComponent<Rigidbody>();
                     rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
                     rb.AddForce(transform.up * 8f, ForceMode.Impulse);
                 }
                 else
                 {
-                    MechaController.Instance.DamagePart(damage);
+                    //TODO MechaController.Instance.DamagePart(damage);
                 }
-                
+
+                animator.SetTrigger(rnd == 0 ? AttackLeft : AttackRight);
                 _alreadyAttacked = true;
                 Invoke(nameof(ResetAttack), timeBetweenAttacks);
             }
@@ -99,8 +123,14 @@ namespace _Scripts.Enemies
         public void TakeDamage(int damage)
         {
             _currentHealth -= damage;
-            if (_currentHealth <= 0) Destroy(gameObject);
+            if (_currentHealth <= 0)
+            {
+                animator.SetBool(IsDead, true);
+                Invoke(nameof(Die), 2);
+            }
         }
+
+        private void Die() => Destroy(gameObject);
 
         private void OnDrawGizmosSelected()
         {
